0511a98d3478c2ac86f4f64b96b1c2e5d4957fe4
Brian Meeker <brian@brianmeeker.me>
2023-05-29 15:10:07 -0400

    Create a live view to create a pet with a name
    This just persists data to the DB and redirects the user. Nothing important happens yet

--- lib/live_pet_web/live/new_pet_live.ex
+++ lib/live_pet_web/live/new_pet_live.ex
@@ -1,7 +1,37 @@
 defmodule LivePetWeb.NewPetLive do
   use LivePetWeb, :live_view
 
+  alias LivePet.Pets
+  alias LivePet.Pets.Pet
+
+  @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    pet = %Pet{}
+    changeset = Pets.change_pet(pet)
+    {:ok, socket |> assign(:pet, pet) |> assign_form(changeset)}
+  end
+
+  @impl true
+  def handle_event("validate", %{"pet" => pet_params}, socket) do
+    changeset =
+      socket.assigns.pet
+      |> Pets.change_pet(pet_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign_form(socket, changeset)}
+  end
+
+  def handle_event("save", %{"pet" => pet_params}, socket) do
+    case Pets.create_pet(pet_params) do
+      {:ok, _pet} ->
+        {:noreply, socket |> put_flash(:info, "Pet created successfully") |> redirect(to: ~p"/")}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign_form(socket, changeset)}
+    end
+  end
+
+  defp assign_form(socket, %Ecto.Changeset{} = changeset) do
+    assign(socket, :form, to_form(changeset))
   end
 end
