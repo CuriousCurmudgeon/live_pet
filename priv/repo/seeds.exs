149b238e1477103d499aa9c342e2d570cfdeb928
Brian Meeker <brian@brianmeeker.me>
2023-05-31 22:36:54 -0400

    Seed 10,000 pets at a time
    This currently breaks the app. Every pet trying to update its state in the DB at once breaks things and the supervisor falls over. Need to debug what's going in detail next time.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,20 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias LivePet.Pets.Pet
+alias LivePet.Repo
+
+Repo.delete_all(Pet)
+
+pet_count = 10_000
+chunk_size = 10_000
+
+timestamp = DateTime.utc_now() |> DateTime.to_naive() |> NaiveDateTime.truncate(:second)
+
+1..pet_count
+|> Enum.map(fn i ->
+  %{name: "pet-#{i}", age: 0, user_id: 1, inserted_at: timestamp, updated_at: timestamp}
+end)
+|> Enum.chunk_every(chunk_size)
+|> Enum.each(fn pets -> Repo.insert_all(Pet, pets) end)
