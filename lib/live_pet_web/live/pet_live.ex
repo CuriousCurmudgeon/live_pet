ea69be21f90c706e1ac5175c40cdd7d10b667c60
Brian Meeker <brian@brianmeeker.me>
2023-07-09 21:06:40 -0400

    Redirect the user to the list of all pets if they don't have permission
    Users should only be able to view their own pets.

--- lib/live_pet_web/live/pet_live.ex
+++ lib/live_pet_web/live/pet_live.ex
@@ -2,13 +2,22 @@ defmodule LivePetWeb.PetLive do
   use LivePetWeb, :live_view
 
   alias LivePet.Pets
+  alias LivePet.Pets.Pet
 
   @impl true
   def mount(%{"id" => id}, _session, socket) do
-    # TODO: Make sure the logged in user owns the pet
-    pet = Pets.Server.pet(id)
+    current_user_id = socket.assigns.current_user.id
 
-    {:ok, assign_pet(socket, pet)}
+    case Pets.Server.pet(id) do
+      %Pet{user_id: ^current_user_id} = pet ->
+        {:ok, assign_pet(socket, pet)}
+
+      _ ->
+        {:ok,
+         socket
+         |> put_flash(:error, "You do not have permission to view that pet")
+         |> redirect(to: ~p"/pets")}
+    end
   end
 
   defp assign_pet(socket, pet) do
