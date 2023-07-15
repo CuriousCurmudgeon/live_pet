126df7d4dccd1e84a534a4eb924561b2f7965023
Brian Meeker <brian@brianmeeker.me>
2023-07-15 18:57:01 -0400

    Simplify the tracking of pet information by using a single key
    I just want a list of active pets, so just store them under one key instead of a bunch.

--- lib/live_pet_web/presence.ex
+++ lib/live_pet_web/presence.ex
@@ -2,27 +2,26 @@ defmodule LivePetWeb.Presence do
   use Phoenix.Presence, otp_app: :live_pet, pubsub_server: LivePet.PubSub
 
   alias LivePetWeb.Presence
-  @active_pet_topic "active_pet"
+  @active_pets_topic "active_pets"
+  @active_pets_key "active_pets"
 
   def track_pet(pid, pet) do
-    Presence.track(pid, @active_pet_topic, pet.id, %{pet: pet})
+    Presence.track(pid, @active_pets_topic, @active_pets_key, %{pet: pet})
   end
 
   def untrack_pet(pid, pet) do
-    Presence.untrack(pid, @active_pet_topic, pet.id)
+    Presence.untrack(pid, @active_pets_topic, @active_pets_key)
   end
 
   def update_pet(pid, pet) do
-    Presence.update(pid, @active_pet_topic, pet.id, %{pet: pet})
+    Presence.update(pid, @active_pets_topic, @active_pets_key, %{pet: pet})
   end
 
   def list_active_pets do
-    Presence.list(@active_pet_topic)
-    |> Enum.map(&extract_pets/1)
-  end
-
-  defp extract_pets({_pet_id, %{metas: metas}}) do
-    %{pet: pet} = List.first(metas)
-    pet
+    Presence.list(@active_pets_topic)
+    |> Map.get(@active_pets_key, %{})
+    |> Map.get(:metas, [])
+    |> Enum.map(fn %{pet: pet} -> pet end)
+    |> Enum.uniq_by(fn p -> p.id end)
   end
 end
