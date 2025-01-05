760788468910b17af1a22edc70eff51cbda27953
Brian Meeker <brian.meeker@getthru.io>
2025-01-05 15:39:41 -0500

    Update to Elixir 1.18.1 and Erlang 27.2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule LivePet.MixProject do
     [
       app: :live_pet,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
